3b4d1c53bd29fd833ac667543ef80a36fc0bb3aa
= <=>
2016-01-04 19:02:17 +0000

    sale decides price (still 7.95)

--- Test/source/mh/pos/test/SellOneItemTest.java
+++ Test/source/mh/pos/test/SellOneItemTest.java
@@ -1,5 +1,6 @@
 package mh.pos.test;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -11,21 +12,45 @@ public class SellOneItemTest {
 
     @Test
     public void productFound() throws Exception {
-
-        Sale sale = new Sale();
-        sale.onBarcode("123456");
         Display display = new Display();
+        Sale sale = new Sale(display);
+        sale.onBarcode("123456");
         assertEquals("7.95", display.getText());
     }
 
+    @Test
+    @Ignore("Refactoring...")
+    public void anoterProductFound() throws Exception {
+
+        Display display = new Display();
+        Sale sale = new Sale(display);
+        sale.onBarcode("123458");
+        assertEquals("12.95", display.getText());
+
+    }
+
     private static class Sale {
+        private Display display;
+
+        public Sale(Display display) {
+            this.display = display;
+        }
+
         public void onBarcode(String barcode) {
+            display.setText("7.95");
         }
     }
 
     private static class Display {
+
+        String text;
+
         public String getText() {
-            return "7.95";
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
         }
     }
 }
